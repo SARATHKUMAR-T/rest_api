161c36daef000c78a46ae8eac9e3ce0fee2a441b
Sarath Kumar T <sarathkumartk98@gmail.com>
2024-03-26 14:09:16 +0530

    class changes

--- dist/server.js
+++ dist/server.js
@@ -8,45 +8,43 @@ const cors_1 = __importDefault(require("cors"));
 require("dotenv/config");
 const morgan_1 = __importDefault(require("morgan"));
 const db_connection_1 = require("./db_connection");
-const user_1 = require("./models/user");
-const userRoute_1 = require("./routes/userRoute");
 const app = (0, express_1.default)();
 // middlewares
 app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 // db connection
-db_connection_1.db.connect((err) => {
-    if (err) {
-        console.log("error while connecting db");
-        console.log(err);
-    }
-    console.log("Db connected successfully");
-    // creating db
-    db_connection_1.db.query("CREATE DATABASE IF NOT EXISTS usersDB", (err) => {
-        if (err)
-            console.log(err, "error while creating db");
-        console.log("database created successfully");
-    });
-    // creating user table
-    db_connection_1.db.query("USE usersDB", (err) => {
-        if (err)
-            console.log(err, "error while selecting db");
-        else {
-            console.log("Db selected successfully");
-        }
-        db_connection_1.db.query(user_1.user, (err) => {
-            if (err)
-                console.log(err, "unable to create table");
-            else {
-                console.log("user table creation was successfull");
-            }
-        });
-    });
-});
+// db.connect((err) => {
+//   if (err) {
+//     console.log("error while connecting db");
+//     console.log(err);
+//   }
+//   console.log("Db connected successfully");
+//   // creating db
+//   db.query("CREATE DATABASE IF NOT EXISTS usersDB", (err) => {
+//     if (err) console.log(err, "error while creating db");
+//     console.log("database created successfully");
+//   });
+//   // creating user table
+//   db.query("USE usersDB", (err): void => {
+//     if (err) console.log(err, "error while selecting db");
+//     else {
+//       console.log("Db selected successfully");
+//     }
+//     db.query(user, (err): void => {
+//       if (err) console.log(err, "unable to create table");
+//       else {
+//         console.log("user table creation was successfull");
+//       }
+//     });
+//   });
+// });
+// connecting to db
+const connectingDb = db_connection_1.db.connect();
+console.log(connectingDb, "connceting to db");
 // listening
-app.listen(process.env.PORT, () => {
+app.listen(8000, () => {
     console.log("Server is Listening");
 });
 // Different routes
-app.use("", userRoute_1.userRouter);
+// app.use("", userRouter);
