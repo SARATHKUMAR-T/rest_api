8c9d47d7e956af03e2992764f443ce18c4c633d0
Sarath Kumar T <sarathkumartk98@gmail.com>
2024-03-26 15:40:42 +0530

    db changes

--- src/db_connection.ts
+++ src/db_connection.ts
@@ -1,48 +1,7 @@
 import mysql from "mysql2";
 
-// export const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "123",
-// });
-
-class DB {
-  private connectionStatus: boolean = false;
-  constructor(
-    public host: string,
-    public user: string,
-    public password: string
-  ) {}
-
-  connection() {
-    return new Promise((resolve, reject) => {
-      resolve(
-        mysql.createConnection({
-          host: this.host,
-          user: this.user,
-          password: this.password,
-        })
-      );
-    });
-  }
-
-  connect() {
-    if (!this.connectionStatus) {
-      this.connection()
-        .then((res) => {
-          console.log("Db connected successfully");
-          console.log(res);
-          this.connectionStatus = true;
-        })
-        .catch((err) => {
-          console.log(err);
-          console.log("unable to connect db");
-          return err;
-        });
-    }
-  }
-
-  query(query: string, values: string | string[],setQuery():any) {}
-}
-
-export const db = new DB("localhost", "root", "123");
+export const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "123",
+});
